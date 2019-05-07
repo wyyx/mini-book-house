5f869c09757e85dd14e7e1afbde78ab667e2e6a6
wyyx <gnehcyx@163.com>
2019-05-07 16:47:26 +0800

    enable playing music

--- miniprogram/components/music/music.component.ts
+++ miniprogram/components/music/music.component.ts
@@ -1,5 +1,7 @@
 import { episodeBehavior } from '../behaviors/episode.behavior'
 
+const audioContext = wx.createInnerAudioContext()
+
 Component({
   /**
    * 组件的属性列表
@@ -21,6 +23,11 @@ Component({
       type: String,
       value: './images/player@pause.png',
       optionalTypes: []
+    },
+    musicAudioSrc: {
+      type: String,
+      value: '',
+      optionalTypes: []
     }
   },
 
@@ -29,12 +36,38 @@ Component({
    */
   data: {},
 
+  attached() {
+    const that: any = this
+
+    that.recoverPlayingStatus()
+  },
+
   /**
    * 组件的方法列表
    */
   methods: {
+    recoverPlayingStatus() {
+      const that: any = this
+
+      if (audioContext.src === that.data.musicAudioSrc) {
+        this.setData({
+          isPlaying: true
+        })
+      }
+    },
+
     togglePlay() {
       const that: any = this
+
+      // seting audio src
+      audioContext.src = that.data.musicAudioSrc
+
+      if (that.data.isPlaying) {
+        audioContext.pause()
+      } else {
+        audioContext.play()
+      }
+
       this.setData({
         isPlaying: that.data.isPlaying ? false : true
       })
